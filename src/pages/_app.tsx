79884a9341a30845c02428733698c0e4c5f61c82
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2023-04-27 16:35:20 -0300

    feat: add Head and Headbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,17 +7,30 @@ import {type AppType} from "next/app"
 import {type Session} from "next-auth"
 
 import "@/styles/globals.css"
+import Head from "next/head"
+import {Headbar} from "@/components/Headbar"
 
 const MyApp: AppType<{session: Session | null}> = ({
   Component,
   pageProps: {session, ...pageProps}
 }) => {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-    </MantineProvider>
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <title>Dungeons & Dummies</title>
+      </Head>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <SessionProvider session={session}>
+          <Headbar />
+
+          <Component {...pageProps} />
+        </SessionProvider>
+      </MantineProvider>
+    </>
   )
 }
 
