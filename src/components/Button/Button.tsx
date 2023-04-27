80b90f5b8249eabd9d15dd80d8fa4d0da8553e52
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2023-04-27 18:54:52 -0300

    refact: type of children from any to ReactNode

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,6 @@
 import useStyles from "./Button.style"
 
-const Sheet = ({children}: {children: any}) => {
+const Sheet = ({children}: {children: React.ReactNode}) => {
   const {classes} = useStyles()
   return <button className={classes.button}>{children}</button>
 }
