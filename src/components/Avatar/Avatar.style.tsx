dcd8664681102b9e11ecfb0e057c8f057f7b8ab7
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2023-04-27 19:47:08 -0300

    feat: create alpha avatar

--- src/components/Avatar/Avatar.style.tsx
+++ src/components/Avatar/Avatar.style.tsx
@@ -1,5 +1,16 @@
-import {createStyles} from "@mantine/core"
+import {createStyles, rem} from "@mantine/core"
 
-const useStyles = createStyles({})
+const useStyles = createStyles({
+  avatar: {
+    width: rem(256),
+    height: rem(256),
+    position: "relative",
+    borderRadius: rem(2000)
+  },
+  image: {
+    objectFit: "cover",
+    borderRadius: rem(2000)
+  }
+})
 
 export default useStyles
--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,8 +1,17 @@
+import NatImage from "../Image/Image"
 import useStyles from "./Avatar.style"
 
 const Avatar = () => {
   const {classes} = useStyles()
-  //
+  return (
+    <div className={classes.avatar}>
+      <NatImage
+        className={classes.image}
+        alt=""
+        src="https://picsum.photos/200"
+      />
+    </div>
+  )
 }
 
 export default Avatar
