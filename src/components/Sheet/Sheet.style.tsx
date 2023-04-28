8c03884ca7dd659cc29fbe6b4b86ea9244263eed
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2023-04-28 16:44:46 -0300

    feat: add partial sheet

--- src/components/Sheet/Sheet.style.tsx
+++ src/components/Sheet/Sheet.style.tsx
@@ -1,5 +1,33 @@
-import {createStyles} from "@mantine/core"
+import {createStyles, rem} from "@mantine/core"
 
-const useStyles = createStyles(theme => ({}))
+const useStyles = createStyles(theme => ({
+  sheet: {
+    paddingInline: "5vw",
+    marginTop: rem(24)
+  },
+  picture: {
+    [theme.fn.smallerThan("md")]: {
+      margin: "auto"
+    }
+  },
+  charHead: {
+    [theme.fn.smallerThan("md")]: {
+      justifyContent: "space-between"
+    }
+  },
+  firstInfo: {
+    width: "40vw"
+  },
+  status: {
+    justifyContent: "space-between",
+    width: "100%"
+  },
+  lvl: {
+    width: "47%"
+  },
+  xp: {
+    width: "47%"
+  }
+}))
 
 export default useStyles
--- src/components/Sheet/Sheet.tsx
+++ src/components/Sheet/Sheet.tsx
@@ -1,12 +1,78 @@
+import {
+  Container,
+  Group,
+  NumberInput,
+  Stack,
+  TextInput,
+  Textarea
+} from "@mantine/core"
+
 import {Avatar} from "../Avatar"
+
 import useStyles from "./Sheet.style"
 
 const Sheet = () => {
   const {classes} = useStyles()
+
   return (
-    <>
-      <Avatar />
-    </>
+    <Container className={classes.sheet}>
+      <Group>
+        <Group>
+          <Avatar
+            alt=""
+            src="https://picsum.photos/250"
+            className={classes.picture}
+          />
+
+          <Group className={classes.status}>
+            <NumberInput
+              label="Level"
+              defaultValue={1}
+              min={1}
+              max={50}
+              className={classes.lvl}
+              hideControls
+              variant="unstyled"
+            />
+            <NumberInput
+              label="Experiência"
+              defaultValue={0}
+              min={0}
+              className={classes.xp}
+              hideControls
+              variant="unstyled"
+            />
+          </Group>
+        </Group>
+
+        <Group className={classes.charHead}>
+          <TextInput
+            className={classes.firstInfo}
+            label="Nome"
+            placeholder="Nome do personagem"
+            variant="unstyled"
+          />
+          <TextInput
+            className={classes.firstInfo}
+            label="Ancestralidade"
+            placeholder="Ancest. do personagem"
+            variant="unstyled"
+          />
+          <TextInput
+            className={classes.firstInfo}
+            label="Vocação"
+            placeholder="Vocação do personagem"
+            variant="unstyled"
+          />
+          <TextInput
+            className={classes.firstInfo}
+            label="Servo Fiel"
+            placeholder="Entidade adorada pelo personagem"
+            variant="unstyled"
+          />
+        </Group>
+      </Group>
+    </Container>
   )
 }
 
