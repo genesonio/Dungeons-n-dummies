1fba93a78f40c66347e1dd6d2ae168bb12d0a1e5
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2023-04-28 14:37:32 -0300

    style: override TextInput styles

--- src/utils/mantineTheme.ts
+++ src/utils/mantineTheme.ts
@@ -5,6 +5,18 @@ const MyTheme: MantineThemeOverride = {
     sm: "rgba(0, 0, 0, 0.3) 2px 3px 4px 2px",
     md: "rgba(0, 0, 0, 0.3) 2px 3px 8px 4px",
     xs: "rgba(0, 0, 0, 0.3) 2px 3px 2px 1px"
+  },
+  components: {
+    Input: {
+      styles: () => ( {
+        input: { color: '#6666f1', "&::placeholder": { color: "rgba(102, 102, 241, 0.3)" } }
+      } )
+    },
+    InputWrapper: {
+      styles: () => ( {
+        label: { color: '#696969' }
+      } )
+    }
   }
 }
 
