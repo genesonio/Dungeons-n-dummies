d1976fc714c2ae7295710a026f9870159bcd0872
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2023-05-02 17:52:55 -0300

    feat: add more status hp, sp, status bar to hp and sp, add str, dex, int, char and modifiers inputs and start skill section

--- src/components/Sheet/Sheet.style.tsx
+++ src/components/Sheet/Sheet.style.tsx
@@ -3,7 +3,10 @@ import {createStyles, rem} from "@mantine/core"
 const useStyles = createStyles(theme => ({
   sheet: {
     paddingInline: "5vw",
-    marginTop: rem(24)
+    marginTop: rem(24),
+    display: "flex",
+    flexDirection: "column",
+    gap: rem(8)
   },
   picture: {
     width: rem(192),
@@ -11,7 +14,27 @@ const useStyles = createStyles(theme => ({
     margin: "auto",
     borderRadius: "9999px"
   },
-  charHead: {
+  stats: {
+    width: "100%"
+  },
+  bars: {
+    width: "40%",
+    ":nth-last-child(-n + 1)": {
+      color: theme.colors.grape[1],
+      fontSize: rem(14),
+      fontWeight: "bold"
+    }
+  },
+  sp: {
+    width: "25%"
+  },
+  attributes: {
+    width: "22%",
+    color: theme.colors.grape[1],
+    fontSize: rem(14),
+    fontWeight: "bold"
+  },
+  baseInfo: {
     justifyContent: "space-between"
   },
   firstInfo: {
--- src/components/Sheet/Sheet.tsx
+++ src/components/Sheet/Sheet.tsx
@@ -1,26 +1,43 @@
-import {useState} from "react"
+import {useEffect, useState} from "react"
 
-import {Container, Group, NumberInput, TextInput} from "@mantine/core"
-
-import {Avatar} from "../Avatar"
+import {
+  Avatar,
+  Container,
+  Group,
+  Loader,
+  NumberInput,
+  Progress,
+  ScrollArea,
+  Stack,
+  Text,
+  TextInput
+} from "@mantine/core"
 
 import useStyles from "./Sheet.style"
 import expByLvl from "@/utils/levelUtils"
 
+import {character} from "@/utils/tempChar"
+import type {ICharacter} from "@/types/char"
+
 interface IStatus {
   exp: number
-  lvl: number
+  level: number
 }
 
 const Sheet = () => {
-  const [status, setStatus] = useState<IStatus>({
-    lvl: 1,
-    exp: 0
-  })
+  const [status, setStatus] = useState<IStatus>({level: 1, exp: 0})
+
+  const [char, setChar] = useState<ICharacter>(character)
+
+  useEffect(() => {
+    setChar(character)
+    setStatus(character.level)
+  }, [])
+
   const {classes} = useStyles()
 
   const handleChange = (value: number | "") => {
-    if (value === "") return setStatus({lvl: 1, exp: 0})
+    if (value === "") return setStatus({level: 1, exp: 0})
 
     setStatus(prev => ({...prev, exp: value}))
 
@@ -36,67 +53,175 @@ const Sheet = () => {
     }
   }
 
+  if (!char) return <Loader />
+
   return (
     <Container className={classes.sheet}>
-      <Group>
+      <Avatar alt="" variant="outline" src={null} className={classes.picture} />
+
+      <Stack className={classes.stats}>
         <Group>
-          <Avatar
-            alt=""
-            src="https://picsum.photos/250"
-            className={classes.picture}
-          />
-
-          <Group className={classes.status}>
+          <Stack className={classes.bars} spacing="xs">
             <NumberInput
-              label="Level"
-              defaultValue={1}
-              min={1}
-              max={50}
-              className={classes.lvl}
-              hideControls
               variant="unstyled"
-              value={status.lvl}
+              label="HP"
+              value={char.stats.currentHp}
+              max={char.stats.hp}
+              onChange={e =>
+                setChar(prev => ({
+                  ...prev,
+                  stats: {
+                    ...prev.stats,
+                    currentHp: e === "" ? 0 : e
+                  }
+                }))
+              }
             />
-            <NumberInput
-              label="Experiência"
-              defaultValue={0}
-              min={0}
-              className={classes.xp}
-              hideControls
-              variant="unstyled"
-              value={status.exp}
-              onChange={value => handleChange(value)}
+            <Progress
+              label={
+                char.stats.currentHp.toString() +
+                " / " +
+                char.stats.hp.toString()
+              }
+              color="red"
+              size="xl"
+              value={(100 / char.stats.hp) * char.stats.currentHp}
             />
-          </Group>
+          </Stack>
+          <Stack className={classes.bars} spacing="xs">
+            <Group>
+              <NumberInput
+                className={classes.sp}
+                variant="unstyled"
+                label="SP"
+                value={char.stats.currentSp}
+                max={char.stats.sp}
+                onChange={e =>
+                  setChar(prev => ({
+                    ...prev,
+                    stats: {
+                      ...prev.stats,
+                      currentSp: e === "" ? 0 : e
+                    }
+                  }))
+                }
+              />
+              <Text component="p">+ {Math.floor(5 / 3)}</Text>
+            </Group>
+            <Progress
+              color="blue"
+              label={
+                char.stats.currentSp.toString() +
+                " / " +
+                char.stats.sp.toString()
+              }
+              size="xl"
+              value={(100 / char.stats.sp) * char.stats.currentSp}
+            />
+          </Stack>
         </Group>
 
-        <Group className={classes.charHead}>
-          <TextInput
-            className={classes.firstInfo}
-            label="Nome"
-            placeholder="Nome do personagem"
-            variant="unstyled"
-          />
-          <TextInput
-            className={classes.firstInfo}
-            label="Ancestralidade"
-            placeholder="Ancest. do personagem"
-            variant="unstyled"
-          />
-          <TextInput
-            className={classes.firstInfo}
-            label="Vocação"
-            placeholder="Vocação do personagem"
-            variant="unstyled"
-          />
-          <TextInput
-            className={classes.firstInfo}
-            label="Servo Fiel"
-            placeholder="Entidade adorada pelo personagem"
-            variant="unstyled"
-          />
+        <Group spacing="xs">
+          <Stack className={classes.attributes}>
+            <NumberInput
+              variant="unstyled"
+              label="Força"
+              value={char.stats.str}
+            />
+            <Text component="p">+ {Math.floor(char.stats.str / 3)}</Text>
+          </Stack>
+          <Stack className={classes.attributes}>
+            <NumberInput
+              variant="unstyled"
+              label="Destreza"
+              value={char.stats.dex}
+            />
+            <Text component="p">+ {Math.floor(char.stats.dex / 3)}</Text>
+          </Stack>
+          <Stack className={classes.attributes}>
+            <NumberInput
+              variant="unstyled"
+              label="Inteligência"
+              value={char.stats.int}
+            />
+            <Text component="p">+ {Math.floor(char.stats.int / 3)}</Text>
+          </Stack>
+          <Stack className={classes.attributes}>
+            <NumberInput
+              variant="unstyled"
+              label="Carisma"
+              value={char.stats.char}
+            />
+            <Text component="p">+ {Math.floor(char.stats.char / 3)}</Text>
+          </Stack>
         </Group>
+      </Stack>
+
+      <Group className={classes.baseInfo}>
+        <NumberInput
+          className={classes.firstInfo}
+          label="Level"
+          defaultValue={1}
+          min={1}
+          max={50}
+          hideControls
+          variant="unstyled"
+          value={status.level}
+        />
+        <NumberInput
+          className={classes.firstInfo}
+          label="Experiência"
+          defaultValue={0}
+          min={0}
+          hideControls
+          variant="unstyled"
+          value={status.exp}
+          onChange={value => handleChange(value)}
+        />
+        <TextInput
+          className={classes.firstInfo}
+          label="Nome"
+          placeholder="Nome do personagem"
+          variant="unstyled"
+          value={char.name}
+          onChange={e => setChar(prev => ({...prev, name: e.target.value}))}
+        />
+        <TextInput
+          className={classes.firstInfo}
+          label="Ancestralidade"
+          placeholder="Ancest. do personagem"
+          variant="unstyled"
+          value={char.race}
+          onChange={e => setChar(prev => ({...prev, race: e.target.value}))}
+        />
+        <TextInput
+          className={classes.firstInfo}
+          label="Vocação"
+          placeholder="Vocação do personagem"
+          variant="unstyled"
+          value={char.role}
+          onChange={e => setChar(prev => ({...prev, role: e.target.value}))}
+        />
+        <TextInput
+          className={classes.firstInfo}
+          label="Servo Fiel"
+          placeholder="Entidade adorada pelo personagem"
+          variant="unstyled"
+          value={char.divinity}
+          onChange={e => setChar(prev => ({...prev, divinity: e.target.value}))}
+        />
       </Group>
+
+      <ScrollArea.Autosize
+        scrollHideDelay={500}
+        scrollbarSize={8}
+        offsetScrollbars
+        className={classes.skills}
+      >
+        {character.skills.map(skill => (
+          <p key={skill.id}>{skill.name}</p>
+        ))}
+      </ScrollArea.Autosize>
     </Container>
   )
 }
