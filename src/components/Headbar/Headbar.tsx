8b767cde5ffead5c5b7790284ae90f1917085cb3
Caio Roberto Hach <darkcaiobits@gmail.com>
2023-05-27 06:27:17 -0300

    adds login/logoff out behaviour to Headbar.tsx

--- src/components/Headbar/Headbar.tsx
+++ src/components/Headbar/Headbar.tsx
@@ -3,20 +3,36 @@ import useStyles from "./Headbar.style"
 import {Button} from "../Button"
 import Link from "next/link"
 import {Image} from "../Image"
+import { useSession, signIn , signOut} from "next-auth/react";
 
 const Sheet = () => {
-  const {classes} = useStyles()
+  const {classes} = useStyles(),
+  { data:sessionData } = useSession();
+  if(!sessionData){
+    return (
+      <Box className={classes.nav} component="nav">
+        <Link className={classes.logoWrapper} href="/">
+          <Image className={classes.logo} alt="" src="/logo.svg" />
+        </Link>
+        <Group noWrap>
+          <Button onClick={() => {signIn()}}>Login</Button>
+          <Button>Sign Up</Button>
+        </Group>
+      </Box>
+    )
+  }
+
   return (
     <Box className={classes.nav} component="nav">
       <Link className={classes.logoWrapper} href="/">
         <Image className={classes.logo} alt="" src="/logo.svg" />
       </Link>
       <Group noWrap>
-        <Button>Login</Button>
-        <Button>Sign Up</Button>
+        <Button onClick={() => {signOut()}}>Logout</Button>
       </Group>
     </Box>
   )
+
 }
 
 export default Sheet
